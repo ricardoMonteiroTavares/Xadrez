1d566f7c8db38f5f0767b25e1b8b9e6984c37861
Ricardo Tavares <ricardotavares51@hotmail.com>
2021-02-15 20:34:54 -0300

    Criação da função que converte um código em hexadecimal para um rgb table

--- Xadrez/menu.lua
+++ Xadrez/menu.lua
@@ -36,6 +36,30 @@ local function onPlayBtnRelease()
 	return true	-- indicates successful touch
 end
 
+-- Função que converte um código de cor em hexadecimal sem o # em um código de cor decimal
+-- String hex -> Cor em hexadecimal sem #
+-- Retorno: rgb table
+local function color(hex)
+	assert(type(hex) == "string" ,"O código de cor deve ser do tipo string")
+	
+	if string.len(hex) == 6 then
+		local r = tonumber(string.sub(hex,1,2), 16)/255
+		local g = tonumber(string.sub(hex,3,4), 16)/255
+		local b = tonumber(string.sub(hex,5,6), 16)/255
+
+		return { r, g, b }
+	
+	elseif string.len(hex) == 3 then
+		local r = (tonumber(string.sub(hex, 1, 1), 16) * 17)/255
+		local g = (tonumber(string.sub(hex, 2, 2), 16) * 17)/255
+		local b = (tonumber(string.sub(hex, 3, 3), 16) * 17)/255
+
+		return { r, g, b }
+	else
+		error("O código de cor deve ter apenas 3 ou 6 caracteres.")
+	end
+end
+
 -- Função que cria os botões
 -- String title -> título do botão
 -- int 	  y_pos -> posição do botão no eixo y
